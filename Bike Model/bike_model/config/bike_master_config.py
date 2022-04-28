61db7c757c2090f588ac8f1e0ef96e4f13276e67
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-28 13:20:38 +0800

    Delete Bike Model directory

--- Bike Model/bike_model/config/bike_master_config.py
+++
@@ -1,8 +0,0 @@
-import os
-from route_model.config.master_config import MasterConfig
-
-class BikeMasterConfig(MasterConfig):
-	"""compile configuration data"""
-	
-	def __init__(self, changes={}):
-		MasterConfig.__init__(self,changes)
